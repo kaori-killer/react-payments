0edd4f18a6dfc45cdec4ef1fd856349695ebee50
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-04-02 16:54:58 +0900

    버튼 크기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import CardList from './components/card-list/CardList';
+import CardManage from './components/card-manage/CardManage';
 import CardsProvider from './context/CardsProvider';
-
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-import routes from './routes';
-
-const router = createBrowserRouter(routes);
-
-// function Stepper() {
-//   const { currentStep, goNextStep, goPrevStep } = useStepper();
-
-//   return (
-//     <div>
-//       <h2>현재 스텝: {currentStep}</h2>
-//       <button onClick={goPrevStep}>이전</button>
-//       <button onClick={goNextStep}>다음</button>
-//     </div>
-//   );
-// }
 
 export default function App() {
   return (
     <CardsProvider>
-      {/* <StepperProvider> */}
-      {/* <Stepper /> */}
-      <RouterProvider router={router} />
-      {/* </StepperProvider> */}
+      <CardManage />
     </CardsProvider>
   );
 }
